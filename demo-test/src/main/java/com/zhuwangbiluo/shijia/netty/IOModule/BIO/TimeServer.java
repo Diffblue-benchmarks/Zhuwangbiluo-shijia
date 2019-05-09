421d32656bf6730f593f25a180ab60eccb393409
释迦 <zhuwangbiluo@qq.com>
2019-05-09 13:59:02 +0800

    BIO test

---
+++ demo-test/src/main/java/com/zhuwangbiluo/shijia/netty/IOModule/BIO/TimeServer.java
@@ -0,0 +1,41 @@
+package com.zhuwangbiluo.shijia.netty.IOModule.BIO;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * TimeServer
+ *
+ * @author shijia.lym
+ * @since 2019-05-07
+ */
+public class TimeServer {
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (Exception e) {
+                //采用默认值
+            }
+        }
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("The time server is start in port：" + port);
+            Socket socket = null;
+            while (true) {
+                socket = serverSocket.accept();
+                new Thread(new TimeServerHandler(socket)).start();
+            }
+        } finally {
+            if (serverSocket != null) {
+                System.out.println("The time server close!");
+                serverSocket.close();
+                serverSocket = null;
+            }
+        }
+
+    }
+}
